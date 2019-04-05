e426c55be79fe0ac0ccacb2a40f5cb90a0ad9ea8
Bakuenjin <julian.zameit.96@gmail.com>
2019-04-06 00:41:23 +0200

    cleaned up constants, added actual useful stuff

--- util/Constants.js
+++ util/Constants.js
@@ -1,80 +1,226 @@
 'use strict'
 
-exports.fskTypes = [
-    'FSK0',
-    'FSK6',
-    'FSK12',
-    'FSK16',
-    'FSK18',
-    'BAD_LANGUAGE',
-    'VIOLENCE',
-    'FEAR',
-    'SEX'
-]
+exports.API_BASE = "https://proxer.me/api/v1/"
 
-exports.sortTypes = [
-    'RELEVANCE',
-    'CLICKS',
-    'RATING',
-    'COUNT',
-    'NAME'
-]
-
-exports.tagRateFilterTypes = {
-    DEFINED_TAGS: 'rate_1',
-    INCLUDE_UNDEFINED_TAGS: 'rate_10'
+exports.classes = {
+    USER: 'user',
+    LIST: 'list',
+    INFO: 'info',
+    UCP: 'ucp',
+    NOTIFICATIONS: 'notifications',
+    MEDIA: 'media',
+    ANIME: 'anime',
+    MANGA: 'manga',
+    MESSENGER: 'messenger',
+    FORUM: 'forum',
+    APPS: 'apps',
+    CHAT: 'chat',
+    user: {
+        LOGIN: 'login',
+        LOGOUT: 'logout',
+        USERINFO: 'userinfo',
+        TOP_TEN: 'topten',
+        LIST: 'list',
+        LATEST_COMMENTS: 'comments',
+        HISTORY: 'history',
+        ABOUT: 'about',
+        REQUEST_AUTHENTIFICATION: 'requestauth',
+        CHECK_AUTHENTIFICATION: 'checkauth',
+        FRIENDS: 'friends'
+    },
+    list: {
+        ENTRY_SEARCH: 'entrysearch',
+        ENTRY_LIST: 'entrylist',
+        TAG_IDS: 'tagids',
+        TAGS: 'tags',
+        TRANSLATOR_GROUPS: 'translatorgroups',
+        INDUSTRIES: 'industrys',
+        TRANSLATOR_GROUP_PROJECTS: 'translatorgroupprojects',
+        INDUSTRY_PROJECTS: 'industryprojects',
+        CHARACTERS: 'characters',
+        PERSONS: 'persons'
+    },
+    info: {
+        FULL_ENTRY: 'fullentry',
+        ENTRY: 'entry',
+        NAMES: 'names',
+        GATE: 'gate',
+        LANGUAGES: 'lang',
+        SEASON: 'season',
+        GROUPS: 'groups',
+        PUBLISHER: 'publisher',
+        LIST_INFO: 'listinfo',
+        COMMENTS: 'comments',
+        RELATIONS: 'relations',
+        ENTRY_TAGS: 'entrytags',
+        ENTRY_GENRES: 'entrygenres',
+        TRANSLATOR_GROUP: 'translatorgroup',
+        INDUSTRY: 'industry',
+        RECOMMENDATIONS: 'recommendations',
+        SET_USERINFO: 'setuserinfo',
+        GET_USERINFO: 'userinfo',
+        CHARACTERS: 'characters',
+        CHARACTER: 'character',
+        PERSONS: 'persons',
+        PERSON: 'person',
+        FORUM: 'forum'
+    },
+    ucp: {
+        LIST: 'list',
+        LIST_SUM: 'listsum',
+        TOP_TEN: 'topten',
+        HISTORY: 'history',
+        VOTES: 'votes',
+        REMINDER: 'reminder',
+        SET_REMINDER: 'setreminder',
+        DELETE_REMINDER: 'deletereminder',
+        DELETE_FAVORITE: 'deletefavorite',
+        DELETE_VOTE: 'deletevote',
+        SET_COMMENT_STATE: 'setcommentstate',
+        DELETE_COMMENT: 'deletecomment',
+        SETTINGS: 'settings',
+        SET_SETTINGS: 'setsettings'
+    },
+    notifications: {
+        COUNT: 'count',
+        NEWS: 'news',
+        GET: 'notifications',
+        DELETE: 'delete'
+    },
+    media: {
+        RANDOM_HEADER: 'randomheader',
+        HEADER_LIST: 'headerlist',
+        CALENDAR: 'calendar',
+        VAST_TAG: 'vasttag'
+    },
+    anime: {
+        STREAMS: 'streams',
+        PROXER_STREAMS: 'proxerstreams',
+        LINK: 'link',
+        LINK_WITH_VAST: 'linkvast'
+    },
+    manga: {
+        CHAPTER: 'chapter'
+    },
+    messenger: {
+        CONSTANTS: 'constants',
+        CONFERENCES: 'conferences',
+        CONFERENCE_INFO: 'conferenceinfo',
+        USER_INFO: 'userinfo',
+        MESSAGES: 'messages',
+        NEW_CONFERENCE: 'newconference',
+        NEW_CONFERENCE_GROUP: 'newconferencegroup',
+        REPORT: 'report',
+        SET_MESSAGE: 'setmessage',
+        SET_READ: 'setread',
+        SET_UNREAD: 'setunread',
+        SET_BLOCK: 'setblock',
+        SET_UNBLOCK: 'setunblock',
+        SET_FAVORITE: 'setfavour',
+        UNSET_FAVORITE: 'setunfavour'
+    },
+    forum: {
+        TOPIC: 'topic'
+    },
+    apps: {
+        ERROR_LOG: 'errorlog'
+    },
+    chat: {
+        ROOM_INFO: 'roominfo',
+        ROOM_USERS: 'roomusers',
+        PUBLIC_ROOMS: 'publicrooms',
+        MY_ROOMS: 'myrooms',
+        MESSAGES: 'messages',
+        NEW_MESSAGES: 'newmessages',
+        NEW_MESSAGE: 'newmessage',
+        DELETE_MESSAGE: 'deletemessage',
+        REPORT_MESSAGE: 'reportmessage',
+        THANKYOU_MESSAGE: 'thankyoumessage'
+    }
 }
 
-exports.tagSpoilerFilterTypes = {
-    NO_SPOILER: 'spoiler_0',
-    SPOILER: 'spoiler_1',
-    SPOILER_AND_NO_SPOILER: 'spoiler_10'
-}
-
-exports.categoryTypes = [
-    'ANIME',
-    'MANGA'
-]
-
-exports.seasonTypes = [
-    'START',
-    'END'
-]
-
-exports.sortOptionTypes = [
-    'TITLE',
-    'CLICKS',
-    'RATING'
-]
-
-exports.sortTypes = [
-    'ASC',
-    'DESC'
-]
-
-exports.animeSeasonTypes = {
-    UNKNOWN: 0,
-    WINTER: 1,
-    SPRING: 2,
-    SUMMER: 3,
-    FALL: 4
-}
-
-exports.mediumTypes = [
-    'ANIMESERIES',
-    'MOVIE',
-    'OVA',
-    'HENTAI',
-    'MANGASERIES',
-    'ONESHOT',
-    'DOUJIN',
-    'HMANGA'
-]
-
-exports.stateTypes = {
-    PRE: 0,
-    DONE: 1,
-    ONGOING: 2,
-    QUIT: 3,
-    SUBOPEN: 4
+exports.errors = {
+    FATAL: {
+        "1000": "API version does not exist.",
+        "1001": "API version was removed.",
+        "1002": "API class does not exist.",
+        "1003": "API function does not exist.",
+        "1004": "The API keys does not own the privilege to execute this action.",
+        "1005": "The used login token is invalid.",
+        "1006": "The called function is locked.",
+        "1007": "Proxer.me is currently undergoing maintenance.",
+        "1008": "The API is currently undergoing maintenance."
+    },
+    SERVER_SIDE: {
+        "2000": "IP was blocked by firewall.",
+        "2001": "Error while fetching news."
+    },
+    CLIENT_SIDE: {
+        "3000": "Login failed: Missing login credentials.",
+        "3001": "Login failed: Invalid login credentials.",
+        "3002": "Fetching notifications failed: User is not logged in.",
+        "3003": "Fetching userinfo failed: User id does not exist.",
+        "3004": "UCP error: User is not logged in.",
+        "3005": "UCP error: Category does not exist.",
+        "3006": "UCP error: Invalid id.",
+        "3007": "Info error: Invalid id.",
+        "3008": "Setting userinfo failed: Invalid type.",
+        "3009": "Setting userinfo failed: User is not logged in.",
+        "3010": "Setting userinfo failed: Content already exists in list.",
+        "3011": "Setting userinfo failed: Favorite limit exceeded.",
+        "3012": "Login failed: User is already logged in.",
+        "3013": "Login failed: Another user is already logged in.",
+        "3014": "User error: Access to information denied. (This might be caused by not beeing logged in with a user)",
+        "3015": "List error: Category does not exist.",
+        "3016": "List error: Medium does not exist.",
+        "3017": "Media error: Style does not exist.",
+        "3018": "Media error: Entry does not exist.",
+        "3019": "Manga error: Chapter does not exist. (Maybe it simply is not uploaded or linked yet)",
+        "3020": "Anime error: Episode does not exist. (Maybe is simply is not uploaded or linked yet)",
+        "3021": "Anime error: Stream does not exist.",
+        "3022": "UCP error: Episode does not exist.",
+        "3023": "Messages error: User is not logged in.",
+        "3024": "Messages error: Invalid conference. (missing permissions or invalid id)",
+        "3025": "Messages error: Invalid or missing input.",
+        "3026": "Messages error: Invalid or missing message.", 
+        "3027": "Messages error: Invalid user.",
+        "3028": "Messages error: User limit exceeded.",
+        "3029": "Messages error: Invalid or missing topic.",
+        "3030": "Messages error: At least one user needs to be added to a conference",
+        "3031": "Chat error: Invalid room.",
+        "3032": "Chat error: Missing permissions.",
+        "3033": "Chat error: Invalid message. (this always corresponds to a specific message id)",
+        "3034": "Chat error: User is not logged in.",
+        "3035": "List error: Invalid language.",
+        "3036": "List error: Invalid type.",
+        "3037": "List error: Invalid id.",
+        "3038": "Login failed: The user has 2FA activated. The secret key needs to be specified.",
+        "3039": "Login failed: The user account got terminated.",
+        "3040": "Login failed: The user account is banned.",
+        "3041": "User error: An internal error occured.",
+        "3042": "Apps error: An empty string was sent.",
+        "3043": "List error: Invalid subject.",
+        "3044": "Forum error: Invalid id.",
+        "3045": "Apps error: Invalid id.",
+        "3046": "List error: The top-access list got reset.",
+        "3047": "User-Auth error: The user does not exist.",
+        "3048": "User-Auth error: The code needs to be 100 characters long.",
+        "3049": "User-Auth error: The code already exists.",
+        "3050": "User-Auth error: The code does not exist.",
+        "3051": "User-Auth error: The code got denied.",
+        "3052": "User-Auth error: The code is not approved by the user yet.",
+        "3053": "User-Auth error: The name can't be empty.",
+        "3054": "User-Auth error: The code is already in use.",
+        "3055": "Chat error: The user needs to exist for at least 7 days to aquire the permission to write in chat.",
+        "3056": "Chat error: The user is blacklisted.",
+        "3057": "Chat error: Missing permissions.",
+        "3058": "Chat error: A user can't thank themself, baka.",
+        "3059": "Chat error: Invalid text input.",
+        "3060": "Forum error: The user does not have the permission to access this area.",
+        "3061": "Info error: Deletion of a comment failed.",
+        "3062": "UCP error: Applying changes failed.",
+        "3063": "Anime error: The stream is not accessable for guests.",
+        "3064": "User error: IP authentications needs to be done.",
+        "3065": "Media error: There aren't any VAST tags available currently."
+    }
 }
