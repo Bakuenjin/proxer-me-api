72ac363460f8476fbebc2018d87c560b9b93bb4a
Julian Zameit <julian.zameit@aerosoft.de>
2019-04-05 16:36:07 +0200

    included fetch for the api request handling later

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// NOTE TO MYSELF:
+// const headers = {
+//     "proxer-api-key": "api-key",
+//     "Content-Type": "application/x-www-form-urlencoded"
+// }
+
+// const body = "key=value"
