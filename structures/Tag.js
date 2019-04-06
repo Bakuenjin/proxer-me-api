ee22df4ca19a62768f915eb88388d6237d7e29ba
Bakuenjin <julian.zameit.96@gmail.com>
2019-04-06 18:02:56 +0200

    Tag class added

---
+++ structures/Tag.js
@@ -0,0 +1,44 @@
+'use strict'
+
+class Tag {
+    constructor(data) {
+        if (data) this.data = data
+    }
+
+    /**
+     * The id of the tag
+     * @type {number}
+     * @readonly
+     */
+    get id() { return parseInt(this.data.id) }
+
+    /**
+     * The type of the tag
+     * @type {string}
+     * @readonly
+     */
+    get type() { return this.data.type }
+
+    /**
+     * The name of the tag
+     * @type {string}
+     * @readonly
+     */
+    get name() { return this.data.tag }
+
+    /**
+     * A small description for this tag
+     * @type {string}
+     * @readonly
+     */
+    get description() { return this.data.description }
+
+    /**
+     * Which subtype this tag belongs to
+     * @type {string}
+     * @readonly
+     */
+    get subType() { return this.data.subtype }
+}
+
+module.exports = Tag
