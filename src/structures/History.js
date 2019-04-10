935701686e01fb1e6192aa88309d60b10c195d2c
Bakuenjin <julian.zameit.96@gmail.com>
2019-04-10 15:47:25 +0200

    added history and topten-item class for users

---
+++ src/structures/History.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const Base = require('./Base')
+const Anime = require('./Anime')
+const Manga = require('./Manga')
+
+class History extends Base {
+    constructor(client, data) {
+        super(client)
+        if (data) this.data = data
+    }
+
+    /**
+     * The unique ID of the history entry
+     * @type {number}
+     * @readonly
+     */
+    get id() { return parseInt(this.data.id) }
+
+    /**
+     * The unique ID of the content
+     * @type {number}
+     * @readonly
+     */
+    get contentId() { return parseInt(this.data.eid) }
+
+    /**
+     * The name of the content this history entry represents
+     * @type {string}
+     * @readonly
+     */
+    get name() { return this.data.name }
+
+    /**
+     * The language of the viewed content
+     * @type {string}
+     * @readonly
+     */
+    get language() { return this.data.language }
+
+    /**
+     * The viewed episode/chapter of the content
+     * @type {string}
+     * @readonly
+     */
+    get viewedEpisode() { return this.data.episode }
+
+    /**
+     * The medium type of this history entry
+     * @type {string}
+     * @readonly
+     */
+    get medium() { return this.data.medium }
+
+    /**
+     * The category type of the content this entry represents
+     * @type {string}
+     * @readonly
+     */
+    get category() { return this.data.category }
+
+    /**
+     * Gathers the information about this content
+     * @returns {Promise<(Anime|Manga)>}
+     */
+    getContent() { return this.client.getContentById(this.contentId) }
+}
+
+module.exports = History
---
+++ src/structures/TopTenItem.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const Base = require('./Base')
+const Anime = require('./Anime')
+const Manga = require('./Manga')
+
+class TopTenItem extends Base {
+    constructor(client, data) {
+        super(client)
+        if (data) this.data = data
+    }
+
+    /**
+     * The unique ID of this top ten content element
+     * @type {number}
+     * @readonly
+     */
+    get id() { return parseInt(this.data.eid) }
+
+    /**
+     * The name of the top ten content
+     * @type {string}
+     * @readonly
+     */
+    get name() { return this.data.name }
+
+    /**
+     * The category of the top ten content
+     * @type {string}
+     * @readonly
+     */
+    get category() { return this.data.kat }
+
+    /**
+     * The medium of the top ten content
+     * @type {string}
+     * @readonly
+     */
+    get medium() { return this.data.medium }
+
+    /**
+     * Gathers the information about this content
+     * @returns {Promise<(Anime|Manga)>}
+     */
+    getContent() { return this.client.getContentById(this.id) }
+}
+
+module.exports = TopTenItem
