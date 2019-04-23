2dc4a3fbc65e5b7a7db807f54d794f9951c66296
Bakuenjin <julian.zameit.96@gmail.com>
2019-04-23 22:39:08 +0200

    moved old APIManager functionality into httpclient class

---
+++ src/http/HttpClient.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const { API_BASE } = require('../util/Constants')
+
+const request = require('./RequestHandler')
+const requestBuilder = require('./RequestBuilder')
+const UrlBuilder = require('./UrlBuilder')
+
+class HttpClient {
+    constructor(headerSettings = { apiKey: false, apiToken: false, testMode: false }) {
+        this.urlBuilder = new UrlBuilder(API_BASE)
+        this.defaultHeaders = { "Content-Type": "application/x-www-form-urlencoded" }
+
+        if (headerSettings.apiKey) this.defaultHeaders["proxer-api-key"] = headerSettings.apiKey
+        else if (headerSettings.testMode) this.defaultHeaders["proxer-api-testmode"] = 1
+        else throw new Error("Can't initialize API connection with neither an API key nor test mode enabled.")
+
+        if(headerSettings.apiToken) this.defaultHeaders['proxer-api-token'] = headerSettings.apiToken
+    }
+
+    /**
+     * Constructs an API call via 'POST'.
+     * @param {string} urlClass - The first chunk of the API call
+     * @param {string} urlFunction - The second chunk of the API call
+     * @param {Object} body - The payload for the API call
+     * @returns {Promise<Object>}
+     */
+    post(urlClass, urlFunction, body = {}) {
+        const url = this.urlBuilder.build(urlClass, urlFunction)
+        const requestParams = requestBuilder.postRequest(this.defaultHeaders, body)
+        return request(url, requestParams)
+    }
+
+    /**
+     * Constructs an API call via 'GET'
+     * @param {string} urlClass - The first chunk of the API call
+     * @param {string} urlFunction - The second chunk of the API call
+     * @param {Object} queryValues - THe query values for the API call
+     * @returns {Promise<Object>}
+     */
+    get(urlClass, urlFunction, queryValues = {}) {
+        const url = this.urlBuilder.build(urlClass, urlFunction)
+        const queryString = UrlBuilder.generateQueryString(queryValues)
+
+        const queryUrl = url + "?" + queryString
+        const requestParams = requestBuilder.getRequest(this.defaultHeaders)
+        return request(queryUrl, requestParams)
+    }
+
+}
+
+module.exports = HttpClient
