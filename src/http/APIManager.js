9ba7ae83110db73bce83f9706a94b17dab7b277b
Bakuenjin <julian.zameit.96@gmail.com>
2019-04-23 22:39:37 +0200

    updated APIManager structure

--- src/http/APIManager.js
+++ src/http/APIManager.js
@@ -1,50 +1,24 @@
 'use strict'
 
-const { API_BASE } = require('../util/Constants')
-
-const request = require('./RequestHandler')
-const requestBuilder = require('./RequestBuilder')
-const UrlBuilder = require('./UrlBuilder')
+const HttpClient = require('./HttpClient')
+const AnimeAPI = require('./apis/AnimeAPI')
+const AppsAPI = require('./apis/AppsAPI')
+const ChatAPI = require('./apis/ChatAPI')
+const ForumAPI = require('./apis/ForumAPI')
+const MangaAPI = require('./apis/MangaAPI')
+const MediaAPI = require('./apis/MediaAPI')
+const MessengerAPI = require('./apis/MessengerAPI')
 
 class APIManager {
-    constructor(apiParams = { apiKey: false, apiToken: false, testMode: false }) {
-        this.urlBuilder = new UrlBuilder(API_BASE)
-        this.defaultHeaders = { "Content-Type": "application/x-www-form-urlencoded" }
-
-        if (apiParams.apiKey) this.defaultHeaders["proxer-api-key"] = apiParams.apiKey
-        else if (apiParams.testMode) this.defaultHeaders["proxer-api-testmode"] = 1
-        else throw new Error("Can't initialize API connection with neither an API key nor test mode enabled.")
-
-        if(apiParams.apiToken) this.defaultHeaders['proxer-api-token'] = apiParams.apiToken
-    }
-
-    /**
-     * Constructs an API call via 'POST'.
-     * @param {string} urlClass - The first chunk of the API call
-     * @param {string} urlFunction - The second chunk of the API call
-     * @param {Object} body - The payload for the API call
-     * @returns {Promise<Object>}
-     */
-    post(urlClass, urlFunction, body = {}) {
-        const url = this.urlBuilder.build(urlClass, urlFunction)
-        const requestParams = requestBuilder.postRequest(this.defaultHeaders, body)
-        return request(url, requestParams)
-    }
-
-    /**
-     * Constructs an API call via 'GET'
-     * @param {string} urlClass - The first chunk of the API call
-     * @param {string} urlFunction - The second chunk of the API call
-     * @param {Object} queryValues - THe query values for the API call
-     * @returns {Promise<Object>}
-     */
-    get(urlClass, urlFunction, queryValues = {}) {
-        const url = this.urlBuilder.build(urlClass, urlFunction)
-        const queryString = UrlBuilder.generateQueryString(queryValues)
-
-        const queryUrl = url + "?" + queryString
-        const requestParams = requestBuilder.getRequest(this.defaultHeaders)
-        return request(queryUrl, requestParams)
+    constructor(headerParams = { apiKey: false, apiToken: false, testMode: false }) {
+        const httpClient = new HttpClient(headerParams)
+        this.anime = new AnimeAPI(httpClient)
+        this.apps = new AppsAPI(httpClient)
+        this.chat = new ChatAPI(httpClient)
+        this.forum = new ForumAPI(httpClient)
+        this.manga = new MangaAPI(httpClient)
+        this.media = new MediaAPI(httpClient)
+        this.messenger = new MessengerAPI(httpClient)
     }
 }
 
