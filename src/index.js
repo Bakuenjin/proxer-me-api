80ab70463d4198f55150ff43a7c9ba281a878366
Bakuenjin <julian.zameit.96@gmail.com>
2019-05-05 21:52:53 +0200

    test area cleanup

--- src/index.js
+++ src/index.js
@@ -1,50 +1,42 @@
 'use strict'
 
-const Client = require('./structures/Client')
-const UserClient = require('./structures/UserClient')
 const APIManager = require('./http/APIManager')
-const { classes, paramConstants } = require('./util/Constants')
+const { paramConstants } = require('./util/Constants')
 
 /**
-* Connect to the proxer.me API without beeing logged in.
-* @param {string} apiKey - The api key to connect to the API with
-* @returns {Client}
-*/
-function connect(apiKey) {
-    return new Client({ apiKey: apiKey })
-}
-
-/**
- * Logs the user into Proxer.Me and returns a client with elevated rights and functionality.
- * @param {string} apiKey - The key for accessing the API
- * @param {string} username - The username for logging in
- * @param {string} password - The password for logging in
- * @param {object} optionalValues - Contains all optional params
- * @param {string} [optionalValues.secretkey] - The 2FA key for logging in
- * @returns {Promise<UserClient>}
+ * Connects to the Proxer.me API.
+ * 
+ * @example
+ * // Connect with an API key
+ * const client = ProxerMe.connect({apiKey = "YOUR_KEY"})
+ * 
+ * // Connect with an API key and a logged in user
+ * const client = ProxerMe.connect({apiKey = "YOUR_KEY", apiToken = "USER_TOKEN"})
+ * 
+ * // Connect in test mode
+ * const client = ProxerMe.connect()
+ * @param {object} connectionOptions - Contains details about the headers.
+ * 
+ * When **customHttpClient** is set, eveything else gets ignored and the customHttpClient is used.
+ * @param {string} [connectionOptions.apiKey] - The unique API key that allows you to access the official Proxer.me API.
+ * 
+ * If you don't have a valid API key and leave this blank, the Proxer.me API will be used in test mode with very limited access.
+ * In case you want a valid API key head over to https://proxer.me/wiki/Proxer_API.
+ * @param {string} [connectionOptions.apiToken] - The token used for communicating with the Proxer.me API as a user.
+ * 
+ * If no user is logged in, the UCP API should not be used.
+ * @param {string} [connectionOptions.userAgent] - This overrides the default userAgent set by the HttpClient.
+ * 
+ * This should be used to identify the app you are building. 
+ * It makes it easier for Proxer.me to analysize what traffic is coming from which application.
+ * @param {HttpClient} [connectionOptions.customHttpClient] - **Overrides the default HttpClient!**
+ * 
+ * Defining a custom httpClient will result in all other parameters to be ignored.
+ * This might be needed if your application needs to set some custom headers or casts to a mock server for testing.
  */
-function login(apiKey, username, password, optionalValues = {}) {
-    return new Promise((resolve, reject) => {
-        const api = new APIManager({ apiKey: apiKey })
-        optionalValues.username = username
-        optionalValues.password = password
-        api.post(classes.USER, classes.user.LOGIN, optionalValues).then((data) => {
-            const apiParams = { apiKey: apiKey }
-            if (data.token) apiParams.apiToken = data.token
-            resolve(new UserClient(apiParams, data))
-        }).catch(reject)
-    })
-}
-
-/**
- * NO API KEY REQUIRED! Connects to the proxer.me API in test mode.
- * @returns {Client}
- */
-function testMode() {
-    return new Client({ testMode: true })
+function connect(connectionOptions = {}) {
+    return new APIManager(connectionOptions)
 }
 
 exports.connect = connect
-exports.login = login
-exports.testMode = testMode
 exports.CONSTANTS = paramConstants
